fb608f14bcb1fc5f184295f8eb19be595d25e480
YnaOjeda <yna.ojeda@gmail.com>
2024-12-09 22:39:03 +1300

    feat: make app blue-themed

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,31 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './styles/main.less'
 import PrimeVue from 'primevue/config'
+import Aura from '@primevue/themes/aura'
+import { definePreset } from '@primevue/themes'
 
-createApp(App).use(PrimeVue).mount('#app')
+const app = createApp(App)
+
+app.use(PrimeVue, {
+  theme: {
+    preset: definePreset(Aura, {
+      semantic: {
+        primary: {
+          50: '{blue.50}',
+          100: '{blue.100}',
+          200: '{blue.200}',
+          300: '{blue.300}',
+          400: '{blue.400}',
+          500: '{blue.500}',
+          600: '{blue.600}',
+          700: '{blue.700}',
+          800: '{blue.800}',
+          900: '{blue.900}',
+          950: '{blue.950}',
+        },
+      },
+    }),
+  },
+})
+
+app.mount('#app')
