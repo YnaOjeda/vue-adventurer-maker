a550ecfdcc669fc48da52707e1dcb4efa894d2fc
YnaOjeda <yna.ojeda@gmail.com>
2024-12-09 22:41:11 +1300

    feat: link up selector to avatar maker

--- src/pages/AvatarMaker.vue
+++ src/pages/AvatarMaker.vue
@@ -1,29 +1,119 @@
 <template>
   <div class="page-container">
     <div class="banner">
-      <div class="circle">
-        <Adventurer :size="'100%'" />
-      </div>
+      <Adventurer
+        :size="'100%'"
+        :face="face"
+        :marking="selectedFeatures.marking"
+        :mouth="selectedFeatures.mouth"
+        :eyes="selectedFeatures.eyes"
+        :brows="selectedFeatures.brows"
+        :glasses="selectedFeatures.glasses"
+        :earrings="selectedFeatures.earrings"
+        :hair="selectedFeatures.hair"
+      />
     </div>
+
+    <Tabs :value="FeatureTabs[0]" scrollable>
+      <TabList>
+        <Tab
+          v-for="tab in FeatureTabs"
+          :key="`feature-tab-${tab}`"
+          :value="tab"
+          >{{ tab }}</Tab
+        >
+      </TabList>
+      <TabPanels>
+        <TabPanel
+          v-for="tab in FeatureTabs"
+          :key="`feature-tab-panel-${tab}`"
+          :value="tab"
+        >
+          <FeatureSelector
+            :feature-key="tab"
+            :feature-data="selectedFeatures[tab] as FeatureBaseProps"
+            @set-variant="setVariant"
+          />
+        </TabPanel>
+      </TabPanels>
+    </Tabs>
   </div>
 </template>
 
 <script setup lang="ts">
-import { Adventurer } from 'vue-adventurer'
+import { ref } from 'vue'
+import { Adventurer, FeatureKey } from 'vue-adventurer'
+import type {
+  AdventurerProps,
+  FeatureType,
+  FeatureBaseProps,
+} from 'vue-adventurer'
+import { FeatureTabs } from '@/constants'
+import Tabs from 'primevue/tabs'
+import TabList from 'primevue/tablist'
+import Tab from 'primevue/tab'
+import TabPanels from 'primevue/tabpanels'
+import TabPanel from 'primevue/tabpanel'
+import FeatureSelector from '@/components/FeatureSelector.vue'
+
+const face = ref<AdventurerProps['face']>({})
+
+const selectedFeatures = ref({
+  [FeatureKey.hair]: {
+    variant: 'hair-2',
+    color: undefined,
+  },
+  [FeatureKey.eyes]: {
+    variant: 'eye-1',
+    color: undefined,
+  },
+  [FeatureKey.brows]: {
+    variant: 'brow-1',
+    color: undefined,
+  },
+  [FeatureKey.mouth]: {
+    variant: 'mouth-1',
+    color: undefined,
+  },
+  [FeatureKey.glasses]: {
+    variant: 'glasses-1',
+    color: undefined,
+  },
+  [FeatureKey.earrings]: {
+    variant: 'earring-1',
+    color: undefined,
+  },
+  [FeatureKey.marking]: {
+    variant: 'marking-1',
+    color: undefined,
+  },
+} as AdventurerProps)
+
+const setVariant = (featureKey: FeatureType, variant?: string) => {
+  if (!variant) {
+    selectedFeatures.value[featureKey] = undefined
+    return
+  }
+
+  if (selectedFeatures.value[featureKey]) {
+    selectedFeatures.value[featureKey].variant = variant
+  }
+}
 </script>
 
 <style lang="less" scoped>
 @import '@/styles/spacing.less';
 @import '@/styles/colors.less';
 
-@banner-height: 150px;
+@banner-height: 300px;
 @banner-width: 100%;
 
 .page-container {
   height: 100vh;
+  background-color: var(--p-content-background);
 
   @media (max-width: @breakpoint-m) {
-    width: 100vh;
+    width: 100vw;
   }
 
   @media (min-width: @breakpoint-m) {
@@ -33,19 +123,9 @@ import { Adventurer } from 'vue-adventurer'
 }
 
 .banner {
-  position: relative;
   width: 100%;
   height: @banner-height;
-  background-color: @gray-1;
   justify-content: center;
   display: flex;
-
-  .circle {
-    background-color: @white;
-    width: 50%;
-    aspect-ratio: 1;
-    position: absolute;
-    border-radius: 50%;
-  }
 }
 </style>
