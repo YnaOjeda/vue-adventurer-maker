b30af12e12310032aa1de051efebbc8cd27662c6
YnaOjeda <yna.ojeda@gmail.com>
2025-06-02 20:57:58 +1200

    feat: set vite config base to reponame

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/vue-adventurer-maker',
   plugins: [vue(), vueJsx(), vueDevTools()],
   resolve: {
     alias: {
